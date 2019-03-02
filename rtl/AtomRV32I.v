0d3f54ce8d78f2b4a17bc613a163088d7e57721a
Udara De Silva <udara28@live.com>
2019-03-02 01:11:38 -0500

    introduced pipeline stages

--- rtl/AtomRV32I.v
+++ rtl/AtomRV32I.v
@@ -1,42 +1,51 @@
 module AtomRV32I #(
     parameter XLEN = 32,
-    parameter REG_ADDR_LEN = $clog2(XLEN)
+    parameter REG_ADDR_LEN = $clog2(XLEN),
+    parameter PC_RESET = 32'h0000_0000
 )(
     input clk,
+    input rst_n,
     input [XLEN-1:0] instr
 );
 
-wire [REG_ADDR_LEN-1: 0] rs1, rs2, rd;
-wire [XLEN-1: 0]         r_data1, r_data2, wr_data, im_data;
-wire [2:0] funct3;
-wire [6:0] funct7;
-wire is_type_R, is_type_I, is_type_S, is_type_B, is_type_U, is_type_J;
-wire is_alu_op;
+/*
+* Program Counter
+*/
+reg [XLEN-1: 0] PC;
+
+
+/*
+* Intermediate wires
+*/
+wire [REG_ADDR_LEN-1: 0] rs1, rs2, rd, wb_rd;
+wire [XLEN-1: 0]         r_data1, r_data2, wdata, im_data;
+wire [2:0]               func3;
+wire [6:0]               func7;
+wire                     is_type_R;
+
+/*
+* pipeline registers
+*
+* NOTE: Named after the pipeline stage the register send data
+*/
+reg [REG_ADDR_LEN-1:0] r_ex_rs1, r_ex_rs2, r_ex_rd;
+reg [XLEN-1:0]         r_ex_inst, r_ex_pc;
+
+reg [REG_ADDR_LEN-1:0] r_mem_rs2;
+reg [XLEN-1:0]         r_mem_wdata, r_mem_inst, r_mem_pc;
+
+reg [REG_ADDR_LEN-1:0] r_wb_rs2;
+reg [XLEN-1:0]         r_wb_wdata, r_wb_inst, r_wb_pc;
+
+/*
+* Module instantiation
+*/
 
 instruction_decoder unit_id(
     .instr(instr),
     .rs1(rs1),
     .rs2(rs2),
     .rd(rd),
-    .funct3(funct3),
-    .funct7(funct7),
-    .is_type_R(is_type_R),
-    .is_type_I(is_type_I),
-    .is_type_S(is_type_S),
-    .is_type_B(is_type_B),
-    .is_type_U(is_type_U),
-    .is_type_J(is_type_J),
-    .is_alu_op(is_alu_op)
-);
-
-immediate unit_imm(
-    .instruction(instr),
-    .is_type_I(is_type_I),
-    .is_type_S(is_type_S),
-    .is_type_B(is_type_B),
-    .is_type_U(is_type_U),
-    .is_type_J(is_type_J),
-    .imm_out(im_data)
 );
 
 register_file unit_regfile(
@@ -44,21 +53,88 @@ register_file unit_regfile(
     .wr_en(1'b0),
     .rs1(rs1),
     .rs2(rs2),
-    .wr_addr(rd),
-    .wr_data(wr_data),
+    .wr_addr(wb_rd),
+    .wr_data(r_wb_wdata),
     .rd_data_1(r_data1),
     .rd_data_2(r_data2)
 );
 
+/*
+* stage #2 pipeline begin (execution)
+*/
+always@(posedge clk) begin
+    if(~rst_n) begin
+        r_ex_pc     <= PC_RESET;
+        r_ex_rs1    <= 32'h0;
+        r_ex_rs2    <= 32'h0;
+        r_ex_inst   <= 32'h0;
+    end else begin
+        r_ex_pc     <= PC;
+        r_ex_rs1    <= rs1;
+        r_ex_rs2    <= rs2;
+        r_ex_instr  <= instr;
+    end
+end
+/*
+* stage #2 pipeline end
+*/
+
+
+immediate unit_imm(
+    .instr(r_ex_instr),
+    .imm_out(im_data)
+);
+
+assign func3 = r_ex_instr[14:12];
+assign func7 = r_ex_instr[31:25];
+assign is_type_R = (r_ex_instr[6:0] == 7'b0110011)? 1'b1 : 1'b0; 
+
 ALU_TOP unit_alu(
-    .PC_IN(32'b0),
-    .RS1_IN(r_data1),
-    .RS2_IN(r_data2),
+    .PC_IN(r_ex_pc),
+    .RS1_IN(r_ex_rs1),
+    .RS2_IN(r_ex_rs2),
     .IMM_IN(im_data),
     .ALU_CTRL({funct3, funct7[5]}),
     .MUX1_CTRL(1'b0),
     .MUX2_CTRL(is_type_R),
-    .ALU_OUT(wr_data)
+    .ALU_OUT(wdata)
 );
+
+/*
+* stage #3 pipeline begin (memory)
+*/
+always@(posedge clk)begin
+    if(~rst_n) begin
+        r_mem_pc    <= PC_RESET;
+        r_mem_rs2   <= 32'h0;
+        r_mem_inst  <= 32'h0;
+        r_mem_wdata <= 32'h0;
+    end else begin
+        r_mem_pc    <= r_ex_pc;
+        r_mem_rs2   <= r_ex_rs2;
+        r_mem_inst  <= r_ex_inst;
+        r_mem_wdata <= wdata;
+    end
+end
+/*
+* stage #3 pipeline end
+*/
+
+/*
+* stage #4 pipeline begin   (write back)
+*/
+always@(posedge clk)begin
+    if(~rst_n) begin
+        r_wb_rs2    <= 32'h0;
+        r_wb_wdata  <= 32'h0;
+        r_wb_inst   <= 32'h0;
+    end else begin
+        r_wb_rs2    <= r_mem_rs2;
+        r_wb_wdata  <= r_mem_wdata;
+        r_wb_inst   <= r_mem_inst;
+    end
+end
+
+assign wb_rd = r_wb_inst[11:7];
 
 endmodule
--- rtl/modules/instruction_decoder/src/instruction_decoder.v
+++ rtl/modules/instruction_decoder/src/instruction_decoder.v
@@ -1,6 +1,5 @@
 module instruction_decoder#(
     parameter XLEN = 32,
-    parameter OP_LEN = 7,
 	parameter REG_FILE_DEPTH = 32,
     parameter REG_FILE_ADDR_LEN = $clog2(REG_FILE_DEPTH)
 )(
@@ -9,31 +8,9 @@ module instruction_decoder#(
     output reg [REG_FILE_ADDR_LEN-1:0] rs1, //read address 1
     output reg [REG_FILE_ADDR_LEN-1:0] rs2, //read address 2
     output reg [REG_FILE_ADDR_LEN-1:0] rd,  //write address
-
-    // Following registers are holding different parts of the instruction
-    // Validity of the data contain in them depends on the tpe of instruction
-    output reg [2:0] funct3,
-    output reg [6:0] funct7,
-
-    // Following registers are indicating the instruction type
-    output reg is_type_R, is_type_I, is_type_S, is_type_B, is_type_U, is_type_J,
-    // special control signals
-    output reg is_alu_op
 );
 
     always @(*) begin
-        is_type_R  = (instr[OP_LEN-1:0] == 7'b0110011);
-        is_type_I  = (instr[OP_LEN-1:0] == 7'b1100111) | (instr[OP_LEN-1:0] == 7'b0000011) | (instr[OP_LEN-1:0] == 7'b0010011);
-        is_type_S  = (instr[OP_LEN-1:0] == 7'b0100011);
-        is_type_B  = (instr[OP_LEN-1:0] == 7'b1100011);
-        is_type_U  = (instr[OP_LEN-1:0] == 7'b0110111) | (instr[OP_LEN-1:0] == 7'b0010111);
-        is_type_J  = (instr[OP_LEN-1:0] == 7'b1101111);
-
-
-        is_alu_op  = (instr[OP_LEN-1:0] == 7'b0110011) | (instr[OP_LEN-1:0] == 7'b0010011);
-
-        funct3 = instr[14:12];
-        funct7 = instr[31:25];
         rd     = instr[11:7];
         rs1    = instr[19:15];
         rs2    = instr[24:20];
